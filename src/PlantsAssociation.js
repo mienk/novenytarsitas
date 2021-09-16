c71c0400eb381f74db339748d89b6f47a841f9c9
lbarjak <barjak.laszlo@gmail.com>
2021-09-16 10:14:03 +0200

    gray added, "I don't know"

--- src/PlantsAssociation.js
+++ src/PlantsAssociation.js
@@ -80,7 +80,7 @@ export default class PlantsAssociation {
     }
 
     #colors = {
-        "x": "maroon", "f": "white", "p": "red", "z": "green"
+        "x": "maroon", "f": "white", "p": "red", "z": "green", "g": "gray"
     }
 
     getPlantsMatrixLength = () => Object.keys(this.#plantsMatrix).length
